d97ebe9828d2b574391bedab8db3cd0b7f40a6d8
yaphet17 <yafetberhanu3@gmail.com>
2022-05-06 16:28:58 +0300

    add tag model

---
+++ src/main/java/com/yaphet/devhackathonabitpain/models/Tag.java
@@ -0,0 +1,20 @@
+package com.yaphet.devhackathonabitpain.models;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name="tags")
+public class Tag {
+    @Id
+    @SequenceGenerator(name = "tag_sequence", sequenceName = "tag_sequence", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "tag_sequence")
+    private Long id;
+    private String tagName;
+}
