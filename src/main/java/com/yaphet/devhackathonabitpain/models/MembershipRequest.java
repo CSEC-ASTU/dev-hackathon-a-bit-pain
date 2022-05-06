5e16bcd6966c6556039173c117dd4044680d167c
yaphet17 <yafetberhanu3@gmail.com>
2022-05-06 19:56:18 +0300

    add membership request model

---
+++ src/main/java/com/yaphet/devhackathonabitpain/models/MembershipRequest.java
@@ -0,0 +1,29 @@
+package com.yaphet.devhackathonabitpain.models;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name="membership_requests")
+public class MembershipRequest {
+
+    @Id
+    @SequenceGenerator(name = "club_sequence", sequenceName = "club_sequence", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "club_sequence")
+    private Long id;
+    @ManyToOne
+    @JoinColumn(nullable = false,name = "app_user_id")
+    private AppUser appUser;
+    @ManyToMany(fetch=FetchType.EAGER)
+    @JoinColumn(nullable = false,name = "division_id")
+    private Set<Division> divisions=new HashSet<>();
+
+}
