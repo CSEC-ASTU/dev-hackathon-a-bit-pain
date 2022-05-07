4286b123dd9fd065bc9b1daecb29c822ded3f681
yaphet17 <yafetberhanu3@gmail.com>
2022-05-07 08:47:41 +0300

    rewrite route

--- src/main/java/com/yaphet/devhackathonabitpain/controllers/ActivationRequestController.java
+++ src/main/java/com/yaphet/devhackathonabitpain/controllers/ActivationRequestController.java
@@ -14,24 +14,28 @@ import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("user/request")
 public class ActivationRequestController {
 
     private final ActivationRequestService activationRequestService;
 
-    @GetMapping("user/request-list")
+    @GetMapping
     public void getAllRequests(Model model){
         List<ActivationRequest> requestList=activationRequestService.findAll();
         model.addAttribute("requestList",requestList);
     }
-    @GetMapping("user/request-list")
-    public void getRequests(@RequestParam("status") Status status, Model model){
+    @GetMapping("/{status}")
+    public void getRequests(@PathVariable("status") Status status, Model model){
         List<ActivationRequest> requestList=activationRequestService.findByStatus(status);
         model.addAttribute("requestList",requestList);
     }
-    @GetMapping("user/request-list")
-    @PostMapping("user/activate")
+    @PostMapping("/activate")
     public void activate(@RequestParam("email") String email){
        activationRequestService.activate(email);
+    }
+    @PostMapping("/decline")
+    public void decline(@RequestParam("email") String email){
+        activationRequestService.decline(email);
     }
 
 
